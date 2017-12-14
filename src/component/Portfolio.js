1efcc1a3ddb0ec9fb6ac849e1c24bec1fd5a587e
fedosov <fedosov@advantum.ru>
2017-12-14 18:14:59 +0300

    Add Routing, Add Bootstrap

--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col } from 'reactstrap';
+import '../css/Portfolio.css';
 
 class Portfolio extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <Row>
+          <Col xs="3">.col-3</Col>
+          <Col xs="4">.col-3</Col>
+          <Col xs="5" >.col-3</Col>
+        </Row>
+      </Container>
     );
   }
 }
